0076f9e9b91e93f867a388dddff7b155c7a777f2
devtrungk3 <ndtrung30@gmail.com>
2025-03-26 16:22:03 +0700

    update database config

--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize({
     dialect: 'postgres',
     host: 'localhost',
     port: 5432,
-    database: process.env.DATABASE_NAME,
+    database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     logging: false
