d55b88e2100ae00e0494feeada59f7bc83e8ca05
NSA-TWBlack <antoin2901@gmail.com>
2025-03-27 22:54:47 +0700

    delete variable and function are never used

--- frontend/src/pages/admin/brand/BrandActions.js
+++ frontend/src/pages/admin/brand/BrandActions.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const BrandActions = ({brand, onView, onEdit, onDelete}) => {
+const BrandActions = ({brand, onEdit, onDelete}) => {
     return (
         <div className="btn-group" role="group" aria-label="Brand actions">
             <button
