57d880f7deb9fa23fd0e665561731e628c93c46e
devtrungk3 <ndtrung30@gmail.com>
2025-03-27 23:25:45 +0700

    update frontend api service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configure axios with a base URL (optional)
 const api = axios.create({
-    baseURL: 'http://localhost:8080', // Replace with your actual base URL
+    baseURL: process.env.REACT_APP_SERVER_URL, // Replace with your actual base URL
     headers: {
         'Content-Type': 'application/json',
     },
